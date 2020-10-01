47fb0fb42558255f923d32b15af7de1c1557f478
Pavel Los <flombik2013@yandex.by>
2020-10-02 01:54:56 +0300

    fix

--- search_app/models.py
+++ search_app/models.py
@@ -1,13 +1,20 @@
-from django.db import models
 import math
+import re
+from typing import Final
+
+import pymorphy2
+from django.db import models
+
+morph: Final = pymorphy2.MorphAnalyzer()
 
 
 # Create your models here.
 
-class Document(models.Model):
-    title = models.CharField(max_length=1000, null=True, default='Title')
-    text = models.CharField(max_length=10000, null=True, default='Sample text')
-    snippet = models.CharField(max_length=300, null=True, default='Sample text')
+class AbstractDocument(models.Model):
+    class Meta:
+        abstract = True
+
+    text = models.TextField(default='Sample text')
 
     def get_words(self):
         words = {}
@@ -16,61 +23,11 @@ class Document(models.Model):
                 word = raw_word[:-1]
             else:
                 word = raw_word
-            if word in words:
-                words[word] += 1
+            word = morph.parse(word)[0].normalized
+            if 'PREP' in word.tag:
+                continue
             else:
-                words[word] = 1
-        return words
-
-    @staticmethod
-    def get_idf():
-        documents = Document.objects.all()
-        n = len(documents)
-        words_count = {}
-        for document in documents:
-            for word in set(document.get_words().keys()):
-                if word in words_count:
-                    words_count[word] += 1
-                else:
-                    words_count[word] = 1
-
-        words_idf = {}
-        for word, p in words_count.items():
-            words_idf[word] = math.log2(n / p)
-        return words_idf
-
-    def get_tf_idf(self):
-        doc_word_dict = self.get_words()
-        max_count = 0
-        for count in doc_word_dict.values():
-            if count > max_count:
-                max_count = count
-        tf_idf_dict = {}
-        for word, idf in self.get_idf().items():
-            if word in doc_word_dict:
-                tf_idf_dict[word] = (doc_word_dict[word] / max_count) * idf
-            else:
-                tf_idf_dict[word] = 0
-        return tf_idf_dict
-
-    def get_length(self):
-        tf_idf_dict = self.get_tf_idf()
-        sum_of_squares = 0
-        for value in tf_idf_dict.values():
-            sum_of_squares += value ** 2
-        return sum_of_squares ** 0.5
-
-
-class Search(models.Model):
-    query = models.CharField(max_length=1000, null=True, default="text")
-
-    def get_words(self):
-        words = {}
-        for raw_word in self.query.split():
-            if raw_word.endswith((',', '.', '-', '!', '?', ';')):
-                word = raw_word[:-1]
-            else:
-                word = raw_word
+                word = word.word
             if word in words:
                 words[word] += 1
             else:
@@ -98,11 +55,52 @@ class Search(models.Model):
             sum_of_squares += value ** 2
         return sum_of_squares ** 0.5
 
+
+class Document(AbstractDocument):
+    title = models.CharField(max_length=1000, null=True, default='Title')
+    snippet = models.CharField(max_length=500, null=True, default='Sample text')
+
+    @staticmethod
+    def get_idf():
+        documents = Document.objects.all()
+        n = documents.count()
+        words_count = {}
+        for document in documents:
+            for word in set(document.get_words().keys()):
+                if word in words_count:
+                    words_count[word] += 1
+                else:
+                    words_count[word] = 1
+
+        words_idf = {}
+        for word, p in words_count.items():
+            words_idf[word] = math.log2(n / p)
+        return words_idf
+
+    def gen_snippet(self, words):
+        snippet = ''
+        for word in words:
+            word = word[:4]
+            pattern = r'.{0,50} %s.{0,50}' % word
+            results = re.findall(pattern, self.text)
+            temp = ''
+            for result in results:
+                temp += '..'
+                if len(temp + result) < 500:
+                    temp += result + '..   '
+            if len(temp) > len(snippet):
+                snippet = temp
+        self.snippet = snippet
+        self.save()
+
+
+class Search(AbstractDocument):
     def get_result(self):
         documents = Document.objects.all()
         docs_sim = {}
         search_tf_idf = self.get_tf_idf()
         for doc in documents:
+            doc.gen_snippet(self.get_words().keys())
             doc_tf_idf = doc.get_tf_idf()
             numerator = 0
             for word in search_tf_idf:
--- search_app/views.py
+++ search_app/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Search, Document
-from bs4 import BeautifulSoup
-import requests
 import re
+
+import requests
+from bs4 import BeautifulSoup
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Search, Document
 
 
 # Create your views here.
 def find_documents():
+    Document.objects.all().delete()
     tut_by_page = requests.get('https://www.tut.by')
     assert tut_by_page.status_code == 200
     soup = BeautifulSoup(tut_by_page.text, 'html.parser')
@@ -25,14 +28,17 @@ def find_documents():
                 for sent in text.split('.'):
                     if len(snippet + sent + '.') <= 300:
                         snippet += sent + '.'
-                Document.objects.update_or_create(title=title, defaults={'text': text, 'snippet': snippet})
+                if Document.objects.count() < 11:
+                    Document.objects.update_or_create(title=title, defaults={'text': text, 'snippet': snippet})
+                else:
+                    return
             except AttributeError:
                 continue
 
 
 def results(request, search_id):
     search = get_object_or_404(Search, pk=search_id)
-    return render(request, 'search_app/results_page.html', {'query': search.query, 'result': search.get_result()})
+    return render(request, 'search_app/results_page.html', {'query': search.text, 'result': search.get_result()})
 
 
 def index(request):
@@ -41,7 +47,7 @@ def index(request):
     if request.method == 'POST':
         find_documents()
         query = request.POST['query']
-        search, _ = Search.objects.get_or_create(query=query)
+        search, _ = Search.objects.get_or_create(text=query)
         return redirect('result', search_id=search.id)
 
 
